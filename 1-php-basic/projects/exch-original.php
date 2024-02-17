71b1f9eeac887cb642dfc47dd4019e1fd04787ad
mackio-0 <minkko000@gmail.com>
2024-02-18 00:49:51 +0630

    Prepare UI for exchange calculator

---
+++ 1-php-basic/projects/exch-original.php
@@ -0,0 +1,75 @@
+<?php include "./template/header.php"; ?>
+<?php include "./template/sidebar.php"; ?>
+
+<section class=" bg-gray-100 p-10 rounded-lg">
+    <!-- Start of Breadcrumb -->
+    <ol class="flex items-center whitespace-nowrap" aria-label="Breadcrumb">
+        <li class="inline-flex items-center">
+            <a class="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500" href="./index.php">
+                Home
+            </a>
+            <svg class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400 dark:text-neutral-600 dark:text-neutral-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="m9 18 6-6-6-6" />
+            </svg>
+        </li>
+        <li class="inline-flex items-center text-sm font-semibold text-gray-800 truncate dark:text-gray-200" aria-current="page">
+            Exchange
+        </li>
+    </ol>
+    <!-- End of Breadcrumb -->
+    <hr class=" border-violet-600 border my-3">
+
+    <?php
+
+    include "./testExc.php";
+
+    ?>
+
+    <form method="post" id="exchangeForm">
+
+    </form>
+    <div class=" mt-3">
+        <label for="amount" class="block text-md font-medium mb-2 dark:text-white">Enter the currency amount.</label>
+        <input type="number" id="amount" form="exchangeForm" name="amount" required class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="">
+    </div>
+
+    <div class="flex gap-3 w-full">
+        <div class="p-5 my-3 flex flex-col gap-3 justify-around w-1/2">
+            <label for="sourceCurrency" class=" text-xl">
+                From
+            </label>
+            <select name="sourceCurrency" id="sourceCurrency" form="exchangeForm" class=" w-full h-10 bg-gray-200 rounded-md">
+                <option value="MMK" class=" text-center">MMK</option>
+                <?php
+                foreach ($exchangeRates as $name => $value) :
+                ?>
+                    <option value="<?= $name ?>" class=" text-center">
+                        <?= $name ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        <div class="p-5 my-3 flex flex-col gap-3 justify-around w-1/2">
+            <label for="targetCurrency" class=" text-xl">
+                To
+            </label>
+            <select name="targetCurrency" id="targetCurrency" form="exchangeForm" class=" w-full h-10 bg-gray-200 rounded-md">
+                <option value="MMK" class=" text-center">MMK</option>
+                <?php
+                foreach ($exchangeRates as $name => $value) :
+                ?>
+                    <option value="<?= $name ?>" class=" text-center">
+                        <?= $name ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+    </div>
+
+    <button type="submit" name="convert" form="exchangeForm" class="py-3 px-4 w-full mt-0 inline-flex items-center justify-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+        Convert
+    </button>
+</section>
+
+
+<?php include "./template/footer.php"; ?>
--- 1-php-basic/projects/exchange.php
+++ 1-php-basic/projects/exchange.php
@@ -19,56 +19,77 @@
     <!-- End of Breadcrumb -->
     <hr class=" border-violet-600 border my-3">
 
-    <?php
+    <section>
 
-    include "./testExc.php";
+        <form action="" id="exchangeForm"></form>
+        <div class="grid grid-cols-2 gap-5">
+            <div class=" col-span-full">
+                <label for="input-label" class="block text-sm font-medium mb-2 dark:text-white">Currency amount</label>
+                <input type="number" id="input-label" name="amount" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600">
+            </div>
+            <div class=" col-span-1">
+                <!-- Select -->
+                <div class="relative">
+                    <select data-hs-select='{
+      "placeholder": "From currency",
+      "toggleTag": "<button type=\"button\"></button>",
+      "toggleClasses": "hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 relative py-3 px-4 pe-9 flex text-nowrap w-full cursor-pointer bg-white border border-gray-200 rounded-lg text-start text-sm focus:border-blue-500 focus:ring-blue-500 before:absolute before:inset-0 before:z-[1] dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600",
+      "dropdownClasses": "mt-2 z-50 w-full max-h-72 p-1 space-y-0.5 bg-white border border-gray-200 rounded-lg overflow-hidden overflow-y-auto dark:bg-slate-900 dark:border-gray-700",
+      "optionClasses": "py-2 px-4 w-full text-sm text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100 dark:bg-slate-900 dark:hover:bg-slate-800 dark:text-gray-200 dark:focus:bg-slate-800",
+      "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"flex-shrink-0 size-3.5 text-blue-600 dark:text-blue-500\" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>"
+    }' class="hidden" name="from_currency">
+                        <option value="">From currency</option>
+                        <option>Name</option>
+                        <option>Email address</option>
+                        <option>Description</option>
+                        <option>User ID</option>
+                    </select>
 
-    ?>
+                    <div class="absolute top-1/2 end-3 -translate-y-1/2">
+                        <svg class="flex-shrink-0 size-3.5 text-gray-500 dark:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="m7 15 5 5 5-5" />
+                            <path d="m7 9 5-5 5 5" />
+                        </svg>
+                    </div>
+                </div>
+                <!-- End Select -->
+            </div>
+            <div class="col-span-1">
+                <!-- Select -->
+                <div class="relative">
+                    <select data-hs-select='{
+      "placeholder": "To currency",
+      "toggleTag": "<button type=\"button\"></button>",
+      "toggleClasses": "hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 relative py-3 px-4 pe-9 flex text-nowrap w-full cursor-pointer bg-white border border-gray-200 rounded-lg text-start text-sm focus:border-blue-500 focus:ring-blue-500 before:absolute before:inset-0 before:z-[1] dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600",
+      "dropdownClasses": "mt-2 z-50 w-full max-h-72 p-1 space-y-0.5 bg-white border border-gray-200 rounded-lg overflow-hidden overflow-y-auto dark:bg-slate-900 dark:border-gray-700",
+      "optionClasses": "py-2 px-4 w-full text-sm text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100 dark:bg-slate-900 dark:hover:bg-slate-800 dark:text-gray-200 dark:focus:bg-slate-800",
+      "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"flex-shrink-0 size-3.5 text-blue-600 dark:text-blue-500\" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>"
+    }' class="hidden" name="to_currency">
+                        <option value="">To Currency</option>
+                        <option>Name</option>
+                        <option>Email address</option>
+                        <option>Description</option>
+                        <option>User ID</option>
+                    </select>
 
-    <form method="post" id="exchangeForm">
-
-    </form>
-    <div class=" mt-3">
-        <label for="amount" class="block text-md font-medium mb-2 dark:text-white">Enter the currency amount.</label>
-        <input type="number" id="amount" form="exchangeForm" name="amount" required class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="">
-    </div>
-
-    <div class="flex gap-3 w-full">
-        <div class="p-5 my-3 flex flex-col gap-3 justify-around w-1/2">
-            <label for="sourceCurrency" class=" text-xl">
-                From
-            </label>
-            <select name="sourceCurrency" id="sourceCurrency" form="exchangeForm" class=" w-full h-10 bg-gray-200 rounded-md">
-                <option value="MMK" class=" text-center">MMK</option>
-                <?php
-                foreach ($exchangeRates as $name => $value) :
-                ?>
-                    <option value="<?= $name ?>" class=" text-center">
-                        <?= $name ?>
-                    </option>
-                <?php endforeach; ?>
-            </select>
+                    <div class="absolute top-1/2 end-3 -translate-y-1/2">
+                        <svg class="flex-shrink-0 size-3.5 text-gray-500 dark:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="m7 15 5 5 5-5" />
+                            <path d="m7 9 5-5 5 5" />
+                        </svg>
+                    </div>
+                </div>
+                <!-- End Select -->
+            </div>
         </div>
-        <div class="p-5 my-3 flex flex-col gap-3 justify-around w-1/2">
-            <label for="targetCurrency" class=" text-xl">
-                To
-            </label>
-            <select name="targetCurrency" id="targetCurrency" form="exchangeForm" class=" w-full h-10 bg-gray-200 rounded-md">
-                <option value="MMK" class=" text-center">MMK</option>
-                <?php
-                foreach ($exchangeRates as $name => $value) :
-                ?>
-                    <option value="<?= $name ?>" class=" text-center">
-                        <?= $name ?>
-                    </option>
-                <?php endforeach; ?>
-            </select>
-        </div>
-    </div>
+        <button type="submit" name="convert" form="exchangeForm" class="py-3 px-4 w-full mt-0 inline-flex items-center justify-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+            Convert
+        </button>
 
-    <button type="submit" name="convert" form="exchangeForm" class="py-3 px-4 w-full mt-0 inline-flex items-center justify-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-        Convert
-    </button>
+    </section>
+
+
+
 </section>
 
 
